3f93968d33a227cd881c160d65fa043e7c2fd929
johnson mwangi <johnsonthuraniramwangi@gmail.com>
2025-02-22 04:11:45 +0300

    Update APIDomain.ts

--- src/utils/APIDomain.ts
+++ src/utils/APIDomain.ts
@@ -1,4 +1,3 @@
 // local host
-export const APIDomain = 'http://localhost:8000/'
-// export const APIDomain = 'https://rental-rides-api.azurewebsites.net'
-//export const APIDomain ='https://wakili-app-api.onrender.com'
+//export const APIDomain = 'http://localhost:8000/'
+export const APIDomain = 'https://wakili-app-api-1.onrender.com'
